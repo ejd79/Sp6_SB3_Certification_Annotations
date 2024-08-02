4e8226cfb84aace456ce3a7d6161ff1fb68bf87a
ejd79 <erickjd79@gmail.com>
2024-08-02 09:27:30 +0200

    using Properties files

--- README.md
+++ README.md
@@ -3,11 +3,27 @@
 ### Annotation Bean Configuration (Stereotype Annotation)
 - Used to create Spring beans automatically in the Application context (Spring IoC container) 
 - The main stereotype annotation is @Component
-- By using @Component, Spring provides more Stereotype meta annotation, such as: @Service, @Repository, @Controller
+
+>By using @Component, Spring provides more Stereotype meta annotation, such as: @Service, @Repository, @Controller
 - @Service is used Spring beans at the Service Layer
 - @Repository is used to create Spring beans for the repositories at the DAO Layer
 - @Controller is used to create Spring beans at the controller layer 
 
+>@value is used: 
+- to passing Default value to properties:  
+    @Value("Default Name")
+    private String defaultName;
+- to read from **application.properties** files:  
+  @Value("${mail.host}")
+  private String host; 
+- to read from **Environment** or **System Variable**  
+  @Value("${java.home}")  
+  private String java_home;  
+  @Value("${HOME}")  
+  private String homeDir;  
+
+> @PropertySource and @PropertySources to provide properties file to Spring Environment
+- is used with Configuration classes
 
 ### Java Bean Configuration
 - The @Bean annotation tells Spring IoC Container to manage VegPizza object 
--- src/main/java/net/javaguides/springannotations/SpringAnnotationsApplication.java
+++ src/main/java/net/javaguides/springannotations/SpringAnnotationsApplication.java
@@ -1,6 +1,11 @@
 package net.javaguides.springannotations;
 
+import net.javaguides.springannotations.controller.MyController;
 import net.javaguides.springannotations.controller.PizzaController;
+import net.javaguides.springannotations.propertysource.PropertySourceDemo;
+import net.javaguides.springannotations.repository.MyRepository;
+import net.javaguides.springannotations.service.MyService;
+import net.javaguides.springannotations.value.ValueAnnotationDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,9 +15,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringAnnotationsApplication {
 
 	public static void main(String[] args) {
+
 		var context = SpringApplication.run(SpringAnnotationsApplication.class, args);
-     	PizzaController pizzaController = (PizzaController) context.getBean("pizzaController");
-		System.out.println(pizzaController.getPizza());
+
+		PropertySourceDemo propertySourceDemo = context.getBean(PropertySourceDemo.class);
+		System.out.println(propertySourceDemo.getEmail());
+		System.out.println(propertySourceDemo.getHost());
+		System.out.println(propertySourceDemo.getPassword());
+		System.out.println(propertySourceDemo.getAppName());
+		System.out.println(propertySourceDemo.getAppDesc());
+
+
+//     	PizzaController pizzaController = (PizzaController) context.getBean("pizzaController");
+//		System.out.println(pizzaController.getPizza());
+
+//		MyController myController = context.getBean(MyController.class);
+//		System.out.println(myController.hello());
+//
+//		MyService myService = context.getBean(MyService.class);
+//		System.out.println(myService.hello());
+//
+//		MyRepository myRepository = context.getBean(MyRepository.class);
+//		System.out.println(myRepository.hello());
+//
+//		ValueAnnotationDemo valueAnnotationDemo = context.getBean(ValueAnnotationDemo.class);
+//		System.out.println(valueAnnotationDemo.getDefaultName());
+//		System.out.println(valueAnnotationDemo.getEmail());
+//		System.out.println(valueAnnotationDemo.getHost());
+//		System.out.println(valueAnnotationDemo.getPassword());
+//		System.out.println(valueAnnotationDemo.getJava_home());
+//		System.out.println(valueAnnotationDemo.getHomeDir());
 
 
 //		VegPizza vegPizza = (VegPizza) context.getBean("vP");
